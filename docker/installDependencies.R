583441895551a8a89c1a49e03ea050a643492c4b
Jochen Weile <jochenweile@gmail.com>
2018-03-13 18:06:28 -0400

    docker setup added

---
+++ docker/installDependencies.R
@@ -0,0 +1,8 @@
+install.packages("devtools",repos="http://cran.us.r-project.org")
+
+library(devtools)
+
+install_github("jweile/yogitools")
+install_github("jweile/hgvsParseR")
+install_github("jweile/rapimave")
+install_github("jweile/mavevis")
