32eb259b6830873e95743c2f86ff35c0321ae34b
Jochen Weile <jochenweile@gmail.com>
2018-03-14 17:29:27 -0400

    test stubs for calcStrucfeats

---
+++ tests/testthat/test_calcStrucFeats.R
@@ -0,0 +1,29 @@
+library("mavevis")
+
+context("caching")
+
+test_that("getCacheFile works",{
+	
+	# alFile <- system.file("testdata","test_alignment.fasta",package="mavevis")
+	
+	result <- getCacheFile("test")
+
+	expect_equal(result,paste0(Sys.getenv("HOME"),"/.mavecache/test"))
+
+})
+
+
+test_that("calc.strucfeats works",{
+	
+	# alFile <- system.file("testdata","test_alignment.fasta",package="mavevis")
+	
+	result <- calc.strucfeats("3UIP","A")
+
+	expect_length(result,21)
+
+	expect_gt(nrow(result),1)
+
+	expect_true(file.exists(getCacheFile("3UIP.pdb")))
+
+})
+
